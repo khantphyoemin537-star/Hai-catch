cdd484dcf779f5091d00d6c06b132081c3f4c0bc
khantphyoemin537-star <khantphyoemin537@gmail.com>
2026-06-10 21:26:36 +0630

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from telethon import TelegramClient, events, types, Button, errors
 # ==========================================
 def f(text):
     """Converts regular English text to premium Bold Sans-Serif Unicode Font"""
-    Normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    normal = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
     bold = "𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵"
     trans = str.maketrans(normal, bold)
     return text.translate(trans)
